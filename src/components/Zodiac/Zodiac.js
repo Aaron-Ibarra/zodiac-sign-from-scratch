927ad916d70e65e2c783d02014d1150ceaf73dde
Aaron Ibarra <aaronibarra132@gmail.com>
2023-01-10 17:08:35 -0800

    fixed up file types for signs and added more styling

--- src/components/Zodiac/Zodiac.js
+++ src/components/Zodiac/Zodiac.js
@@ -4,9 +4,9 @@ import './Zodiac.css';
 export default function Zodiac(props) {
   return (
     <div className="zodiac">
-      <h2>{props.name}</h2>
+      <h2 className="name">{props.name}</h2>
       <img src={`/signs/${props.name}.svg`} />
-      <h3>{props.dates}</h3>
+      <h3 className="dates">{props.dates}</h3>
       <p>{props.symbol}</p>
     </div>
   );
