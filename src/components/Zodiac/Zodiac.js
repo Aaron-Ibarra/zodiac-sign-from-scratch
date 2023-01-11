364127f0a7a1f18bd383d40fdb161a8c0e5a01f1
Aaron Ibarra <aaronibarra132@gmail.com>
2023-01-10 16:48:45 -0800

    zodiac images are loaded dynamically from public directory

--- src/components/Zodiac/Zodiac.js
+++ src/components/Zodiac/Zodiac.js
@@ -5,6 +5,7 @@ export default function Zodiac(props) {
   return (
     <div className="zodiac">
       <h2>{props.name}</h2>
+      <img src={`/signs/${props.name}.svg`} />
       <h3>{props.dates}</h3>
       <p>{props.symbol}</p>
     </div>
